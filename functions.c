b36c8c0433a787d73caa6ff71828995f9bdc88bc
AyunaChampain <151037671+AyunaChampain@users.noreply.github.com>
2024-04-18 23:43:24 +0200

    Test

---
+++ functions.c
@@ -0,0 +1,30 @@
+//
+// Created by ayuna on 18/04/2024.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include "functions.h"
+
+COLUMN *create_column(char* title)
+{
+    COLUMN * p ;
+    COLUMN c = {title, REALOC_SIZE, 0};
+    return &c ;
+}
+
+
+int insert_value(COLUMN* col, int value)
+{
+    int taille = (col -> TL) ;
+    if (taille < REALOC_SIZE)
+    {
+        (col -> val[taille + 1]) = value;
+        col -> TL ++ ;
+        return 1 ;
+    }
+    else
+    {
+        //(col->val) = realloc(col->val, 512);
+        return 0 ;
+    }
+}
--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include "functions.h"
 
 int main() {
     printf("Test Github");
+    COLUMN *mycol = create_column("My column");
+    int val = 5;
+    int test = insert_value(mycol, val) ;
+    if (test == 0)
+        printf("Value added successfully to my column\n");
+    else
+        printf("Error adding value to my column\n");
+
     return 0;
 }
